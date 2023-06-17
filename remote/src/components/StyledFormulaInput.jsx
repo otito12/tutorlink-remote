13d79fb7c02fce359e61aabbf71f071a120f467f
otito12 <titodarluzu@gmail.com>
2023-06-17 14:53:35 +0100

    completed react-mathquill integration

--- remote/src/components/StyledFormulaInput.jsx
+++ remote/src/components/StyledFormulaInput.jsx
@@ -1,7 +1,9 @@
 import "./init";
 import React, { useState } from "react";
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import { addStyles, EditableMathField } from "react-mathquill";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import CancelIcon from "@mui/icons-material/Cancel";
 
 // inserts the required css to the <head> block.
 // // you can skip this, if you want to do that by yourself.
@@ -16,6 +18,7 @@ export default function StyledFormulaInput() {
       sx={{
         border: "1px solid #c1c1c1",
         minWidth: "150px",
+        // position: "relative",
         "& .mq-editable-field": {
           fontSize: "15px",
           border: "none",
@@ -26,7 +29,7 @@ export default function StyledFormulaInput() {
         borderRadius: "5px",
         ":hover": {
           border: "1px solid black",
-          // cursor: "text",
+          cursor: "text",
         },
         "&.mq-root-block .mq-hasCursor": {
           outline: "none",
@@ -34,12 +37,31 @@ export default function StyledFormulaInput() {
         },
       }}
     >
-      <EditableMathField
-        latex={latex}
-        onChange={(mathField) => {
-          setLatex(mathField.latex());
-        }}
-      />
+      <Grid item flex={2} sx={{}}>
+        <Grid container>
+          <EditableMathField
+            latex={latex}
+            onChange={(mathField) => {
+              setLatex(mathField.latex());
+            }}
+          />
+        </Grid>
+      </Grid>
+      <Grid item flex={1}>
+        <Grid
+          justifyContent={"end"}
+          alignContent={"center"}
+          container
+          sx={{
+            p: 1,
+            pr: 1.5,
+            height: "100%",
+          }}
+        >
+          <CheckCircleIcon color="success" />
+          <CancelIcon color="error" />
+        </Grid>
+      </Grid>
     </Grid>
   );
 }
