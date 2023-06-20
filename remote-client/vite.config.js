dc3a91fb59a2bb9a4c76bba7c4854c4480eb714b
otito12 <titodarluzu@gmail.com>
2023-06-20 10:14:15 +0100

    make it better

--- remote-client/vite.config.js
+++ remote-client/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
+// import dotenv from "dotenv";
 
 // https://vitejs.dev/config/
 export default defineConfig({
